d6ea94d878a1601ee19c180b99d91bcc6133c450
Gerald O <gerald.onyango@gmail.com>
2019-01-30 10:23:40 -0500

    adding ability to disable logging

--- src/gentesting.go
+++ src/gentesting.go
@@ -9,11 +9,17 @@ import (
 func main() {
 
 	count, _ := strconv.Atoi(os.Getenv("COUNT"))
+	showlogs := os.Getenv("SHOW_LOGS")
 
 	if count == 0 {
 		count = 500
 	}
+	log.Print(showlogs)
 
+	if showlogs == "false" || showlogs == "FALSE" {
+		log.Print("logging is disabled")
+		return
+	}
 	for i := 0; i < count; i++ {
 		log.Print(i)
 	}
